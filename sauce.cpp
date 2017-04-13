23571e6e591452e397cdfa01ea1b2dde9fa2c243
Ben <benjamin.xavier@mymail.champlain.edu>
2017-04-13 12:21:35 -0400

    Initial COmmit

---
+++ sauce.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <String>
+string welcomeMsg = "Welcome to Shoney's!";
+class szechSauce{
+	void printGotSaucesg(){
+		printf(welcomeMsg,);
+	}
+};
+class customer{
+
+};
+class order{
+
+};
+
+int main(){
+	
+	return 0;
+}
